e66c8391e8f91f4ed3be182e70340ac74307d423
Ethan C <ChumleyEthan1310@gmail.com>
2024-09-23 19:25:09 -0700

    Attempt to make script work

--- snapshotMnt.sh
+++ snapshotMnt.sh
@@ -2,11 +2,13 @@
 
 snapDir="/home/user/Desktop/archive"
 poolName="deadpool"
+datasetNum=0
 
-for snapshot in $snapDir/*.zfs; do
-    datasetName=$(basename "$snapshot" .zfs)
+for snapshot in $snapDir; do
+    datasetName="dataset $datasetNum"
     echo "Receiving snapshot $snapshot into dataset $poolName/$datasetName..."
     sudo zfs receive "$poolName/$datasetName" < "$snapshot"
+    ((datasetNum++))
 done
 
 echo "All snapshots received as individual datasets"
